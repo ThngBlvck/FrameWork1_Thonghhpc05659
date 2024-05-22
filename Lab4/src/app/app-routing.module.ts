d7663e5a204e37d127d093cc04836f0ccc6b0aa0
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-05-22 19:58:11 +0700

    Lab4

---
+++ Lab4/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductdetailComponent } from './pages/product/productdetail/productdetail.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductdetailComponent
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- Lab4/src/app/app.module.ts
+++ Lab4/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ProductdetailComponent } from './pages/product/productdetail/productdetail.component';
 import { StarComponent } from './pages/star/star.component';
 import { FormsModule } from '@angular/forms';
-import {DialogComponent} from './pages/product/dialog.component';
+import { HeaderComponent } from './pages/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    ProductdetailComponent,
     StarComponent,
-    DialogComponent,
+    HeaderComponent,
     HomeComponent
   ],
   imports: [
